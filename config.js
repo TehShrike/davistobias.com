42acf910c0120a52797005b6f9e35cd054b1965e
Tobias Davis <accounts@davistobias.com>
2014-10-26 23:29:34 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+noddityConfig = {
+	title: 'Tobias Davis',
+	noddityRoot: window.location.pathname + 'content/',
+	editLink: 'https://github.com/saibotsivad/saibotsivad.github.io/edit/master/content/',
+	errorPage: '404.md',
+	pathPrefix: '#!/',
+	pagePathPrefix: 'post/',
+	debug: false,
+	sidebar: null
+}
